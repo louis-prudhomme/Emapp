5c3cfe6a79c01b4b1d884a982d9e34f56cb77760
Mélanie <marquesmelanie5@gmail.com>
2019-10-15 09:09:51 +0200

    Commit de base

---
+++ emapp/src/main/java/se/m1/emapp/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package se.m1.emapp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
---
+++ emapp/src/main/java/se/m1/emapp/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package se.m1.emapp.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
